a73e413561428a1d7ee8f51647c6096fb76a6d28
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-29 18:19:37 +0200

    add bonus

---
+++ js/bonus.js
@@ -0,0 +1,73 @@
+"use strict"
+
+Vue.config.devtools = true;
+
+new Vue({
+    el: "#root",
+    data: {
+        listImage: [
+            {
+                url: 'img/01.jpg',
+                title: 'Svezia',
+                subTitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                url: 'img/02.jpg',
+                title: 'Svizzera',
+                subTitle: 'Lorem ipsum',
+            },
+            {
+                url: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                subTitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                url: 'img/04.jpg',
+                title: 'Germania',
+                subTitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                url: 'img/05.jpg',
+                title: 'Paradise',
+                subTitle: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            }
+        ],
+
+        indexActiveImg: 0,
+
+        interval: ""
+    },
+
+    methods: {
+        onClickBackBtn() {
+            this.indexActiveImg--
+
+            if (this.indexActiveImg < 0) {
+                this.indexActiveImg = this.listImage.length - 1
+            }
+        },
+
+        onClickNextBtn() {
+            this.indexActiveImg++
+
+            if (this.indexActiveImg > (this.listImage.length - 1)) {
+                this.indexActiveImg = 0
+            }
+        },
+
+        stopInterval() {
+            clearInterval(this.interval)
+        },
+
+        startInterval() {
+            this.interval = setInterval(this.onClickNextBtn, 3000)
+        }
+    },
+
+    mounted() {
+        this.interval = setInterval(this.onClickNextBtn, 3000)
+    },
+    destroyed() {
+        stopInterval()
+    },
+})
