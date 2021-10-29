33945ec48c2fbba0154752882f6a23d48b465679
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-29 16:16:13 +0200

    add css and js for create dynamic image

--- js/script.js
+++ js/script.js
@@ -3,5 +3,38 @@
 Vue.config.devtools = true;
 
 new Vue({
-    el: "#root"
+    el: "#root",
+    data: {
+        listImage: [
+            {
+                url: 'img/01.jpg',
+                title: 'Svezia',
+                subTitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                url: 'img/02.jpg',
+                title: 'Svizzera',
+                subTitle: 'Lorem ipsum',
+            },
+            {
+                url: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                subTitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                url: 'img/04.jpg',
+                title: 'Germania',
+                subTitle: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                url: 'img/05.jpg',
+                title: 'Paradise',
+                subTitle: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            }
+        ],
+        indexActiveImg: 0
+    },
+    methods: {
+
+    }
 })
